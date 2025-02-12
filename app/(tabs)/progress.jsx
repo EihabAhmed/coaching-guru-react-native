33a2f744601e33dded159c2e4803bb52569b3b00
EihabAhmed <eihabahmed@gmail.com>
2025-02-13 00:11:03 +0200

    User Course Progress Tab

--- app/(tabs)/progress.jsx
+++ app/(tabs)/progress.jsx
@@ -1,10 +1,88 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
+import UserDetailContext from "../../context/UserDetailContext";
+import CourseProgressCard from "../../components/Shared/CourseProgressCard";
+import Colors from "../../constant/Colors";
+import { useRouter } from "expo-router";
 
 export default function Progress() {
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  const [courseList, setCourseList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const route = useRouter();
+
+  useEffect(() => {
+    userDetail && GetCourseList();
+  }, [userDetail]);
+
+  const GetCourseList = async () => {
+    setLoading(true);
+    setCourseList([]);
+    const q = query(
+      collection(db, "Courses"),
+      where("createdBy", "==", userDetail?.email)
+    );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      // console.log("--", doc.data());
+      setCourseList((prev) => [...prev, doc.data()]);
+    });
+    setLoading(false);
+  };
+
   return (
     <View>
-      <Text>Progress</Text>
+      <Image
+        source={require("./../../assets/images/wave.png")}
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: 700,
+        }}
+      />
+
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          padding: 20,
+          marginTop: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 30,
+            color: Colors.WHITE,
+            marginBlock: 10,
+          }}
+        >
+          Course Progress
+        </Text>
+        <FlatList
+          data={courseList}
+          showsVerticalScrollIndicator={false}
+          onRefresh={() => GetCourseList()}
+          refreshing={loading}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              onPress={() =>
+                route.push({
+                  pathname: "/courseView/" + item?.docId,
+                  params: {
+                    courseParams: JSON.stringify(item),
+                  },
+                })
+              }
+            >
+              <CourseProgressCard item={item} width={"96%"} />
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
 }
--- components/Home/CourseProgress.jsx
+++ components/Home/CourseProgress.jsx
@@ -3,14 +3,9 @@ import React from "react";
 import { imageAssets } from "../../constant/Option";
 import Colors from "../../constant/Colors";
 import * as Progress from "react-native-progress";
+import CourseProgressCard from "../Shared/CourseProgressCard";
 
 export default function CourseProgress({ courseList }) {
-  const GetCompletedChapters = (course) => {
-    const completedChapter = course?.completedChapter?.length;
-    const percentage = completedChapter / course?.chapters?.length;
-    return percentage;
-  };
-
   return (
     <View
       style={{
@@ -32,73 +27,8 @@ export default function CourseProgress({ courseList }) {
         horizontal={true}
         showsHorizontalScrollIndicator={false}
         renderItem={({ item, index }) => (
-          <View
-            style={{
-              margin: 7,
-              padding: 15,
-              backgroundColor: Colors.WHITE,
-              borderRadius: 15,
-              width: 280,
-            }}
-          >
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                gap: 8,
-              }}
-            >
-              <Image
-                source={imageAssets[item?.banner_image]}
-                style={{
-                  width: 80,
-                  height: 80,
-                  borderRadius: 8,
-                }}
-              />
-
-              <View
-                style={{
-                  flex: 1,
-                }}
-              >
-                <Text
-                  numberOfLines={2}
-                  style={{
-                    fontFamily: "outfit-bold",
-                    fontSize: 19,
-                    flexWrap: "wrap",
-                  }}
-                >
-                  {item?.courseTitle}
-                </Text>
-                <Text
-                  style={{
-                    fontFamily: "outfit",
-                    fontSize: 15,
-                  }}
-                >
-                  {item?.chapters?.length} Chapters
-                </Text>
-              </View>
-            </View>
-
-            <View
-              style={{
-                marginTop: 10,
-              }}
-            >
-              <Progress.Bar progress={GetCompletedChapters(item)} width={250} />
-              <Text
-                style={{
-                  fontFamily: "outfit",
-                  marginTop: 2,
-                }}
-              >
-                {item?.completedChapter?.length ?? 0} Out of{" "}
-                {item?.chapters?.length} Chapters Completed
-              </Text>
-            </View>
+          <View key={index}>
+            <CourseProgressCard item={item} />
           </View>
         )}
       />
